7a52671a3d36fa64bd295cb665afec9847dfc0b3
Christopher Coco <cacoco@gmail.com>
2024-02-09 19:02:53 -0800

    update README and command help text

--- cmd/validate.go
+++ cmd/validate.go
@@ -36,13 +36,8 @@ func validate(basedir string, writer utils.Writer, inFile string) error {
 // validateCmd represents the validate command
 var validateCmd = &cobra.Command{
 	Use:   "validate",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Validates a codemeta.json file",
+	Long:  `Validates a codemeta.json file. If no input file is specified, the current in progress file will be used.`,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		return validate(utils.UserHomeDir, &utils.StdoutWriter{}, inputFile)
 	},
